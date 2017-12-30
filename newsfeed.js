fb5809e13d07e78c14d30fe3400e53f127d3e550
j-ornelas <johnnyoh1988@gmail.com>
2017-12-30 13:09:06 -0800

    gives the site bare minimum functionality

--- newsfeed.js
+++ newsfeed.js
@@ -1,32 +1,66 @@
 
-        var $body = $('body');
-        $body.html('');
+        var $main = $('.main');
+
 
         $(document).ready(function(){
-        var index = streams.home.length - 1;
-        while(index >= 0){
 
-          var tweet = streams.home[index];
-          var $tweet = $('<div></div>');
-          $tweet.addClass('tweet')
+          $feed = $('<div></div>')
+          $feed.appendTo($main);
 
-          var $user = $('<a></a>');
-          $user.attr({'href': '#', 'class': 'username', 'data-user': tweet.user})
-          $user.text('@' + tweet.user + ': ');
-          $user.appendTo($tweet)
+          var showTweets = function(context){
+            var toDisplay;
+            if(context === 'home'){
+              toDisplay = streams.home
+            } else if (context){
+              toDisplay = streams.users[context]
+            }
 
-          var $message = $('<a></a>');
-          $message.addClass('message')
-          $message.text(tweet.message)
-          $message.appendTo($tweet)
+          $feed.html('');
 
-          var $timestamp = $('<a></a>')
-          $timestamp.addClass('timestamp')
-          $timestamp.text(' ~ ' + tweet.displayDate)
-          $timestamp.appendTo($tweet)
 
-          $tweet.appendTo($body);
+          var index = toDisplay.length - 1;
+          while(index >= 0){
 
-          index -= 1;
+            var tweet = toDisplay[index];
+            var $tweet = $('<div></div>');
+            $tweet.addClass('tweet')
+
+            var $user = $('<a></a>');
+            $user.attr({'href': '#', 'class': 'username', 'data-user': tweet.user})
+            $user.text('@' + tweet.user + ': ');
+            $user.appendTo($tweet)
+
+            var $message = $('<a></a>');
+            $message.addClass('message')
+            $message.text(tweet.message)
+            $message.appendTo($tweet)
+
+            var $hashtag = $('<a></a>')
+
+            var $timestamp = $('<a></a>')
+            $timestamp.addClass('timestamp')
+            $timestamp.text(' ~ ' + tweet.displayDate)
+            $timestamp.appendTo($tweet)
+
+            $tweet.appendTo($feed);
+
+            index -= 1;
+          }
+
+          $('.username').on('click', function (page) {
+            page.preventDefault();
+            showTweets($(this).data('user'));
+          });
+
+          $('.updateButton').on('click', function(page){
+            page.preventDefault();
+            showTweets('home')
+          });
+
         }
+
+
+      showTweets('home')
+
+
       });
